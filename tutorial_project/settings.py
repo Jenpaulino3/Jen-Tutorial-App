148ecc508d4044e3250267f122e94f8dbbf6e355
Jenpaulino3 <jenpaulino3@gmail.com>
2017-09-14 01:34:24 -0400

    maintenance update

--- tutorial_project/settings.py
+++ tutorial_project/settings.py
@@ -19,7 +19,6 @@ SECRET_KEY = 'y*-)o@*mzq((wjb03ybu+#@++@w-m)s)eiq86e74i1dn^9u2y$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False # when you have an error it will give you a log in real life it will be set to false
-TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ['*', 'localhost'] # Add the versions of the urls
 
 # Application definition
